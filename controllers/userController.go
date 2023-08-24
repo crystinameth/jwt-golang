751d0a9bd3a0a8589b35cd61d07481ac30aeaa47
shivimishra <shivimishra130103@gmail.com>
2023-08-24 19:45:07 +0530

    get user and auth done

--- controllers/userController.go
+++ controllers/userController.go
@@ -39,6 +39,24 @@ func GetUsers(){
 
 }
 
-func GetUser(){
+func GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}),Decode(&user)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
 
 }
